9d972ef6d10959e2f9fb02b72e1f8d7f9e96bc22
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-10 14:22:56 +0900

    user8_コントローラーにupdate定義を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
